b49f7bafb21ab2477aee5618f349c591fb210459
eeiaao <eeiaao@gmail.com>
2025-07-14 07:51:42 +0300

    headers

--- src/log/atomic_logger.cpp
+++ src/log/atomic_logger.cpp
@@ -1,4 +1,14 @@
+/*
+ * Flox Engine
+ * Developed by FLOX Foundation (https://github.com/FLOX-Foundation)
+ *
+ * Copyright (c) 2025 FLOX Foundation
+ * Licensed under the MIT License. See LICENSE file in the project root for full
+ * license information.
+ */
+
 #include "flox/log/atomic_logger.h"
+
 #include <cstdio>
 #include <ctime>
 #include <filesystem>
--- src/log/console_logger.cpp
+++ src/log/console_logger.cpp
@@ -1,3 +1,12 @@
+/*
+ * Flox Engine
+ * Developed by FLOX Foundation (https://github.com/FLOX-Foundation)
+ *
+ * Copyright (c) 2025 FLOX Foundation
+ * Licensed under the MIT License. See LICENSE file in the project root for full
+ * license information.
+ */
+
 #include "flox/log/console_logger.h"
 
 #include <chrono>
